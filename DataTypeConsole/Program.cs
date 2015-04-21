8f7fddb4c465792e987c0ef931e0b8a6cf459cff
CODevOp <bretknoll@gmail.com>
2015-04-20 19:26:55 -0600

    Reorganized code into collection and print methods
    Need to write code to get data from user next

--- DataTypeConsole/Program.cs
+++ DataTypeConsole/Program.cs
@@ -8,9 +8,29 @@ namespace DataTypeConsole
 {
     class Program
     {
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            // I contemplated using structs to represent the student information but decided against it.
+
+            GetStudentData();
+
+            GetProfessorData();
+
+            GetUniversityData();
+
+            //ValidateStudenBirthday(new DateTime());
+            
+            /// wait for response before exiting
+            Console.ReadLine();
+        }
+
+        private static void GetStudentData()
+        {
             //Student data
             string studentFirstName;
             string studentLastName;
@@ -24,18 +44,6 @@ namespace DataTypeConsole
             double studentGpa;
             string studentEmail;
 
-            //professor data
-            string professorFirstName;
-            string professorLastName;
-            string professorEmail;
-            //Use an array as another way to structure the data
-            string[] professorCoursesTaught;
-
-            //School data
-            string universityName;
-            string universityProgram;
-
-
             studentFirstName = "Bob";
             studentLastName = "Miller";
             studentBirthdate = new DateTime(1995, 11, 17);
@@ -47,39 +55,64 @@ namespace DataTypeConsole
             studentCountry = "USA";
             studentGpa = 4.0;
             studentEmail = "Bob.Miller@g.com";
-
-
+            PrintStudentData(studentFirstName, studentLastName, studentBirthdate, studentAddressLine1, studentAddressLine2, studentCity, studentStateProvince, studentZipPostal, studentCountry, studentGpa, studentEmail);
+        }
+        private static void GetProfessorData()
+        {
+            //professor data
+            string professorFirstName;
+            string professorLastName;
+            string professorEmail;
+            //Use an array as another way to structure the data
+            string[] professorCoursesTaught;
             professorFirstName = "William";
             professorLastName = "Henderson";
             professorEmail = "WHenderson@mySchool.edu";
             //instantiate and populate array
             professorCoursesTaught = new string[] { "CIS 1010", "CIS 1080" };
 
+            PrintProfessorData(professorFirstName, professorLastName, professorEmail, professorCoursesTaught);
+        }
+        private static void GetUniversityData()
+        {
+            //School data
+            string universityName;
+            string universityProgram;
+            string[] programCoreCourses;
+            string[] programElectiveCourses;
+
             universityName = "Colorado State University";
             universityProgram = "CIS Graduate Program";
 
-            // reference http://biz.colostate.edu/cisMaster/curriculum/Pages/courses.aspx
-            // program Core Courses
-            //CIS600 - IT and Project Management
-            //CIS601 - Enterprise Computing & Systems Integration
-            //CIS605 - Business Visual Application Development
-            //CIS606 - Application Software Infrastructure
-            //CIS610 - Software Development Methodology
-            //CIS611 - Object-Oriented Systems
-            //CIS620 - IT Communication Infrastructure
-            //CIS655 - Business Database Systems
-            //CIS665 - E-Business Application Technology
+            programCoreCourses = new string[]{
+            "CIS600 - IT and Project Management",
+            "CIS601 - Enterprise Computing & Systems Integration",
+            "CIS605 - Business Visual Application Development",
+            "CIS606 - Application Software Infrastructure",
+            "CIS610 - Software Development Methodology",
+            "CIS611 - Object-Oriented Systems",
+            "CIS620 - IT Communication Infrastructure",
+            "CIS655 - Business Database Systems",
+            "CIS665 - E-Business Application Technology",
+            };
 
-            //Elective Courses
-            //BUS690C - Agile Project Management with Scrum
-            //BUS690C - Information Technology Management
-            //BUS690C - Data Visualization and Manipulation
-            //BUS690C - Advanced Networking and Security
-            //CIS570 - Business Intelligence
-            //CIS575 - Applied Data Mining and Analytics in Business
-            //CIS670 - Advanced Project Management and Preparation for the PMP Exam
-            //CIS680A1 - Data, Systems and Network Security
-            //CIS695 - Professional Project or Paper
+            programElectiveCourses = new string[] {
+            "BUS690C - Agile Project Management with Scrum",
+            "BUS690C - Information Technology Management",
+            "BUS690C - Data Visualization and Manipulation",
+            "BUS690C - Advanced Networking and Security",
+            "CIS570 - Business Intelligence",
+            "CIS575 - Applied Data Mining and Analytics in Business",
+            "CIS670 - Advanced Project Management and Preparation for the PMP Exam",
+            "CIS680A1 - Data, Systems and Network Security",
+            "CIS695 - Professional Project or Paper",
+            };
+            
+            PrintUniversityData(universityName, universityProgram, programCoreCourses, programElectiveCourses);
+        }
+
+        private static void PrintStudentData(string studentFirstName, string studentLastName, DateTime studentBirthdate, string studentAddressLine1, string studentAddressLine2, string studentCity, string studentStateProvince, int studentZipPostal, string studentCountry, double studentGpa, string studentEmail)
+        {
 
             //student outputs
             Console.WriteLine("Student");
@@ -91,65 +124,39 @@ namespace DataTypeConsole
             Console.WriteLine("\t{0}", studentAddressLine2);
             Console.WriteLine("\t{0}, {1} {2}", studentCity, studentStateProvince, studentZipPostal);
             Console.WriteLine("\t{0}", studentCountry);
-
+        }
+        private static void PrintProfessorData(string professorFirstName, string professorLastName, string professorEmail, string[] professorCoursesTaught)
+        {
             //profesor outputs
             Console.WriteLine("Professor");
             Console.WriteLine("\tName: {0} {1}", professorFirstName, professorLastName);
             Console.WriteLine("\tEmail: {0}", professorEmail);
             Console.WriteLine("\tCourses Taught:");
-            
-            // loop through each item in the array printing on seperate lines.
-            foreach (string course in professorCoursesTaught.ToList())
-            {
-                Console.WriteLine("\t- {0}", course);
-            }
-
+            PrintSubItems(professorCoursesTaught);
+        }
+        private static void PrintUniversityData(string universityName, string universityProgram, string[] programCoreCourses, string[] programElectiveCourses)
+        {
             // School outputs
             Console.WriteLine("School/University");
             Console.WriteLine("\t{0}", universityName);
             Console.WriteLine("\t{0}", universityProgram);
-
-            /// wait for response before exiting
-            Console.ReadLine();
-
+            Console.WriteLine("Core Courses");
+            PrintSubItems(programCoreCourses);
+            Console.WriteLine("Elective Courses");
+            PrintSubItems(programElectiveCourses);
 
         }
-        public struct Student
+        private static void PrintSubItems(string[] arrayOfSubItems)
         {
-            public string firstName;
-            public string lastName;
-            public DateTime birthdate;
-            public string addressLine1;
-            public string addressLine2;
-            public string city;
-            public string StateProvince;
-            public int zipPostal;
-            public string Country;
-            public double gpa;
+            // loop through each item in the array printing on seperate lines.
+            foreach (string subItem in arrayOfSubItems.ToList())
+            {
+                Console.WriteLine("\t** {0}", subItem);
+            }
         }
-        public struct Professor
+        private static Boolean ValidateStudenBirthday(DateTime studentDateOfBirth)
         {
-            string firstName;
-            string lastName;
-            DateTime birthdate;
-            string addressLine1;
-            string addressLine2;
-            string city;
-            string StateProvince;
-            int zipPostal;
-            string Country;
-            string[] degrees;
-            string[] articles;
-
+            throw new NotImplementedException("The ValidateStudenBirthday Method has not been implemented.");
         }
-        //public struct 
-
-        //    A university degree with pertinent fields such as Bachelor or Master
-        //    A degree can be Bachelor of Science in Information Technology and include fields such as Degree Name, credits required, etc.  Some of the fields, such as course list and prerequisites will need to wait until you know how to create arrays or collections.
-        //    A university program with pertinent fields such as Computer Science or Arts.  Fields might include:
-        //    Program name
-        //    Degrees offered (Bachelor, Master, Ph.D.)
-        //    Department Head
-        //    Information for a course that would be part of your selected degree and program, with pertinent fields
     }
 }
